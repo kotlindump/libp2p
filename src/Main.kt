a7b542fd8c06e4d0b2501a71c1798bb7e34d914c
yejiayu <yejiayu.fe@gmail.com>
2019-06-13 16:57:43 +0800

    chore: Update gradle.

---
+++ src/Main.kt
@@ -0,0 +1,5 @@
+package com.kotlindump.libp2p
+
+fun main() {
+	println("Hello world")
+}
--- src/test/AppTest.kt
+++ src/test/AppTest.kt
@@ -1,3 +1,5 @@
+package com.kotlindump.libp2p.test
+
 import org.junit.Test
 import org.junit.Assert.assertTrue
 
